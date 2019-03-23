f59bd9e6aa80fb3a9569c9c5e73ccdb6281f2232
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-23 02:06:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,5 +34,11 @@ def sum_array(array)
 end
 
 def add_s(array)
-
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      element + "s"
+    else
+      array[1]
+    end
+  end
 end
