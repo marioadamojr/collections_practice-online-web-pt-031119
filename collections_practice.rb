56d951e96bba3d6e8d1e951269abb7116bd4187f
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-23 01:29:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1],array[2] = array[2],array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array[2] = '$'
+  array
+end
+
+def find_a(array)
+  array
+end
